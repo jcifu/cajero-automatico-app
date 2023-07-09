58b8dce1642a5cbc93ee23558f91023e5fa2daf0
jcifu <jcifuentescalderon@gmail.com>
2023-07-08 22:36:33 -0400

    Integración de algunas funciones primordiales al DOM. No hay frontend aún. Creación de sessionStorage y objetos JSON (esta implementación puede verse redundante pero es realizada solo con fines didácticos)

--- js/cajero-automatico.js
+++ js/cajero-automatico.js
@@ -1,164 +1,202 @@
 /* Simulador de cajero automático */
-
-let continuar = true
+const login = document.getElementById("login");
+let continuar = true;
 
 function cancelar() {
-    console.log("Cancelar operación")
-    alert('Operación cancelada. Retire su tarjeta')
-    continuar = false
+    console.log("Cancelar operación");
+    alert("Operación cancelada. Retire su tarjeta");
+    sessionStorage.removeItem('cliente');
+    continuar = false;
 }
 
-function consultarSaldo(saldo) {
-    console.log('Consulta saldo')
-    alert('Su saldo es : $' + saldo)
+function consultarSaldo(idCuenta) {
+    const saldo = idCuenta.saldo;
+    console.log("Consulta saldo: " + saldo);
+    alert("Su saldo es : $" + saldo);
 }
 
 function despedida() {
-    console.log('Despedida')
-    alert('Muchas gracias por preferirnos. Hasta pronto.')
-    continuar = false
+    console.log("Despedida");
+    alert("Muchas gracias por preferir nuestro servicio. Hasta pronto.");
+    continuar = false;
 }
 
 function noDisponible() {
-    console.log('Función no disponible')
-    alert('Función no disponible')
+    console.log("Función no disponible");
+    alert("Función no disponible");
 }
 
 function cambiarClave(cliente) {
-    console.log('Cambiar clave')
-    nuevoPassword = prompt('Ingrese su nuevo número secreto') //en futuras versiones implementar validación clave numérica de 4 dígitos
-    if (id !== null && id !== '') cliente.password = nuevoPassword
-    else alert('Ingrese un número correcto')
-    console.log(cliente.password)
-    alert('Su número secreto ha sido cambiado con éxito.')
+    console.log("Cambiar clave");
+    nuevoPassword = prompt("Ingrese su nuevo número secreto"); //Validación clave numérica de 4 dígitos por implementar...
+    if (id !== null && id !== "") cliente.password = nuevoPassword;
+    else alert("Ingrese un número correcto");
+    console.log(cliente.password);
+    alert("Su número secreto ha sido cambiado con éxito.");
+}
+
+function girarDinero(idCuenta, montoGiro) {
+    let saldo = idCuenta.saldo;
+    console.log("Saldo disponible: $" + saldo);
+    console.log("Monto a girar: $" + montoGiro);
+
+    if (montoGiro <= idCuenta.saldo && montoGiro > 0) {
+        //Giro rápido por 5000 
+        if (montoGiro === 5000) {
+            saldo -= montoGiro;
+            console.log("Giro por $" + montoGiro + ". Saldo restante: $" + saldo);
+            alert("Giro por $" + montoGiro + ". Saldo restante: $" + saldo + ". Retire su dinero y comprobante");
+        } else {
+            //Giro por otro monto 
+            //Se debe separar la funcionalidad de Giro Rápido del Giro por otro monto para poder ingresar el valor después de una validación rápida 
+            montoGiro = prompt("Ingrese monto a girar");
+            saldo -= montoGiro;
+            console.log("Giro por $" + montoGiro + ". Saldo restante: $" + saldo);
+            alert("Giro por $" + montoGiro + ". Saldo restante: $" + saldo + ". Retire su dinero y comprobante");
+        }
+    } else if (montoGiro === 0) alert("El monto a girar no puede ser $0.");
+    else
+        alert("Monto inválido. No puede exceder el saldo disponible ni ser menor a 0.");
+    return saldo;
 }
 
 function cuentaCorriente(cliente) {
-    console.log('Cuenta Corriente')
-    let opcionCorr = prompt('Por favor, seleccione la operación que desea realizar \n 1: Giro rápido por $5000 \n 2: Giro por otro monto \n 3: Consultar Saldo \n 0: Cancelar operación')
-    let montoGiro
-    do {
-        switch (opcionCorr) {
-            case '1':
-                console.log('Giro rápido por $5000')
-                montoGiro = 5000
-                cliente.cuentaCorr.saldo = girarDinero(cliente.cuentaCorr.saldo, montoGiro)
-                nuevaOperacion(cliente)
-                break
-            case '2':
-                console.log('Giro por otro monto')
-                montoGiro = prompt('Ingrese monto a girar')
-                // por agregar: mensaje de error cuando el monto es 0
-                if (montoGiro <= cliente.cuentaCorr.saldo ) cliente.cuentaCorr.saldo = girarDinero(cliente.cuentaCorr.saldo, montoGiro)
-                else alert('Monto inválido. No puede exceder el saldo disponible.') 
-                nuevaOperacion(cliente)
-                break
-            case '3':
-                consultarSaldo(cliente.cuentaCorr.saldo)
-                nuevaOperacion(cliente)
-                break
-            case '0':
-                despedida()
-                continuar = false
-                break
-            case null:
-                cancelar()
-                break
-            default:
-                console.log('Opción no valida')
-                alert('Ingrese una opción válida.')
-                break
-        }
-    } while (continuar === true)
-}
+    let montoGiro;
+    console.log("Cuenta Corriente ID: " + cliente.cuentaCorr.idCuenta);
+    const menuCorr = document.createElement("div");
+    document.body.appendChild(menuCorr);
+    let menuCorrHTML = `
+    <div>
+            <button id="giroRap-btn" type="button">Giro rápido por $5000</button>
+            <button id="giroMonto-btn" type="button">Giro por otro monto</button>
+            <button id="saldo-btn" type="button">Consultar Saldo</button>
+            <button id="salir-btn" type="button">Salir</button>
+        </div>
+    `;
+    menuCorr.innerHTML = menuCorrHTML;
+    menuCorr.id = "menuCorr";
+    const giroRap = document.getElementById("giroRap-btn");
+    const giroMonto = document.getElementById("giroMonto-btn");
+    const consultaSaldo = document.getElementById("saldo-btn");
+    const salir = document.getElementById("salir-btn");
 
-function girarDinero(saldo, montoGiro) {
-    console.log(montoGiro)
-    saldo -= montoGiro
-    console.log('Giro por $' + montoGiro + '. Saldo restante: $' + saldo)
-    alert('Giro por $' + montoGiro + '. Saldo restante: $' + saldo + '. Retire su dinero y comprobante')
-    return saldo
+    //Giro rápido por $5000
+    giroRap.addEventListener("click", () => {
+        montoGiro = 5000;
+        document.getElementById("menuCorr").remove();
+        return girarDinero(cliente.cuentaCorr, montoGiro);
+    });
+    //Giro por otro monto
+    giroMonto.addEventListener("click", () => {
+        document.getElementById("menuCorr").remove();
+        return girarDinero(cliente.cuentaCorr, parseInt(montoGiro));
+    });
+    //Consultar Saldo
+    consultaSaldo.addEventListener("click", () => {
+        document.getElementById("menuCorr").remove();
+        return consultarSaldo(cliente.cuentaCorr);
+    });
+    //Cancelar operación
+    salir.addEventListener("click", () => {
+        document.getElementById("menuCorr").remove();
+        return cancelar();
+    });
 }
 
 function nuevaOperacion(cliente) {
-    continuar = prompt('Desea realizar otra operación? \n 1: Sí \n 2: No')
+    continuar = prompt("Desea realizar otra operación? \n 1: Sí \n 2: No");
     switch (continuar) {
-        case '1':
-            menuPrincipal(cliente)
-            break
-        case '2': despedida()
-            continuar = false
-            break
-        case '':
-            menuPrincipal(cliente)
-            break
+        case "1":
+            menuPrincipal(cliente);
+            break;
+        case "2":
+            despedida();
+            continuar = false;
+            break;
+        case "":
+            menuPrincipal(cliente);
+            break;
         case null:
-            despedida()
-            continuar = false
-            break
-        default: alert('Ingrese una opción válida')
+            despedida();
+            continuar = false;
+            break;
+        default:
+            alert("Ingrese una opción válida");
     }
 }
 
 function menuPrincipal(cliente) {
-    console.log('Acceso cliente id: ' + cliente.id)
-    do {
-        let opcion = prompt('Bienvenido. Por favor, seleccione el producto que desea operar\n 1: Cuenta Corriente\n 2: Cuenta Vista (No disponible) \n 3: Cambiar número secreto\n 0: Salir')
-        switch (opcion) {
-            case '1':
-                cuentaCorriente(cliente)
-                break
-            case '2': 
-            //por agregar: cuentaVista(). Misma lógica que cuentaCorriente()
-                noDisponible()
-                break
-            case '3':
-                cambiarClave(cliente)
-                //Por agregar: despedida alternativa en caso de cambio de clave (evento)
-                despedida() 
-                break
-            case '0':
-                cancelar()
-                break
-            case null:
-                cancelar()
-                break
-            default:
-                alert('Ingrese una opción válida')
-                break
-        }
-    } while (continuar === true)
+    const menu = document.createElement("div");
+    document.body.appendChild(menu);
+    let menuHTML = `
+        <div>
+            <button id="ctaCorr-btn" type="button">Cuenta corriente</button>
+            <button id="ctaVista-btn" type="button">Cuenta Vista</button>
+            <button id="password-btn" type="button">Cambiar clave</button>
+            <button id="salir-btn" type="button">Salir</button>
+        </div>
+     `;
+    menu.innerHTML = menuHTML;
+    menu.id = "menuPrincipal";
+
+    const ctaCorr = document.getElementById("ctaCorr-btn");
+    const ctaVista = document.getElementById("ctaVista-btn");
+    const password = document.getElementById("password-btn");
+    const salir = document.getElementById("salir-btn");
+
+    ctaCorr.addEventListener("click", () => {
+        document.getElementById("menuPrincipal").remove();
+        return cuentaCorriente(cliente);
+    });
+    //No disponible. Por implementar...
+    ctaVista.addEventListener("click", () => {
+        document.getElementById("menuPrincipal").remove();
+        return noDisponible();
+    });
+    //No disponible. Por implementar...
+    password.addEventListener("click", () => {
+        document.getElementById("menuPrincipal").remove();
+        return noDisponible();
+    });
+    salir.addEventListener("click", () => {
+        document.getElementById("menuPrincipal").remove();
+        return cancelar();
+    });
 }
 
+function accesoPrincipal(event) {
+    event.preventDefault();
+    const idCliente = document.getElementById("idCliente").value;
+    const password = document.getElementById("password").value;
+    const parent = login.parentNode;
+    
+    const clientes = JSON.parse(listaClientesJSON);
+    let cliente = clientes.find(
+        (el) => el.id === idCliente && el.password === password
+    );
 
-function accesoPrincipal() {
-    /* 
-    Por mejorar: corregir el comportamiento cuando se desea cancelar la operación durante el ingreso de id y password, se repite 3 veces. 
-    */
+    if (cliente) {
+        parent.removeChild(login);
+        //Almacenamiento y recuperación del cliente en sessionStorage
+        sessionStorage.setItem('cliente', JSON.stringify(cliente));
+        cliente = JSON.parse(sessionStorage.getItem('cliente'));
 
-    let id;
-    id = prompt('Bienvenido. Ingrese número id de cliente')
-    if (id !== null && id !== '') {
-    for (let i = 2; i >= 0; --i) {
-            let password = prompt('Ingrese su password')
-            if (accesoClientes.some((el) => el.id === id && el.password === password)) {
-                const cliente = listaClientes.find((el) => el.id === id)    
-                menuPrincipal(cliente)
-                return; // Salir de la función después de iniciar sesión
-            } else {
-                if (i === 0) {
-                    alert('Número id incorrecto. Intentos restantes: 0.\nSu tarjeta ha sido bloqueada')
-                    console.log('Número id incorrecto. Intentos restantes: 0.\nSu tarjeta ha sido bloqueada')
-                } else {
-                    console.log('Número id incorrecto. Intentos restantes: ' + i)
-                    alert('Número id incorrecto. Intentos restantes: ' + i)
-                }
-            }
-        }
+        alert("Acceso concedido cliente ID: " + cliente.id);
+        console.log("Acceso concedido cliente ID: " + cliente.id);
+        
+        menuPrincipal(cliente); 
     } else {
-        cancelar() // Si el usuario cancela la operación, salir del ciclo
+        //Validacion por usuario invalido por implementar...
+        // Máximo 3 intentos por implementar
+
+        if (cliente) {
+            alert("Acceso concedido cliente ID: " + cliente.id);
+            console.log("Acceso concedido cliente ID: " + cliente.id);
+        } else {
+            alert("Numero de cliente incorrecto. Intente nuevamente");
+            console.log("Numero de cliente incorrecto. Intente nuevamente");
+        }
     }
 }
 
-
-accesoPrincipal()
+login.addEventListener("submit", accesoPrincipal);
--- js/cliente.js
+++ js/cliente.js
@@ -1,49 +1,25 @@
+let id, nombre;
+let cuentaVista, cuentaCorr;
 
-let id, nombre
-
-let cuentaAhorro, cuentaCorr
-
-/* const cliente = {
-    id: '000001',
-    nombre: 'Javier Cifuentes',
-    cuentas : {
-        cuentaAhorro: {
-            numCuenta: '000012',
-            saldo: 500000
-        },
-        cuentaCorr: {
-            numCuenta: '800024',
-            saldo: 750000
-        }
-    }
-} */
-
-
-function Cliente(id, nombre, password, cuentaAhorro, cuentaCorr) {
+function Cliente(id, nombre, password, cuentaVista, cuentaCorr) {
     this.id = id
     this.nombre = nombre
     this.password = password
-    this.cuentaAhorro = {
-        numCuenta: cuentaAhorro.numCuenta,
-        saldo: cuentaAhorro.saldo
+    this.cuentaVista = {
+        idCuenta: cuentaVista.idCuenta,
+        saldo: cuentaVista.saldo
     }
     this.cuentaCorr = {
-        numCuenta: cuentaCorr.numCuenta,
+        idCuenta: cuentaCorr.idCuenta,
         saldo: cuentaCorr.saldo
     }
 }
-const cliente1 = new Cliente('000001', 'Javier', '123456', { numCuenta: '000012', saldo: 400000 }, { numCuenta: '800024', saldo: 750000 });
-const cliente2 = new Cliente('000002', 'María', 'abcdef', { numCuenta: '000034', saldo: 100000 }, { numCuenta: '800045', saldo: 250000 });
-const cliente3 = new Cliente('000003', 'Pedro', 'qwerty', { numCuenta: '000056', saldo: 200000 }, { numCuenta: '800067', saldo: 350000 });
-const cliente4 = new Cliente('000004', 'Ana', 'zxcvbn', { numCuenta: '000078', saldo: 150000 }, { numCuenta: '800089', saldo: 400000 });
 
+const cliente1 = new Cliente('000001', 'Javier', '123456', { idCuenta: '000012', saldo: 400000 }, { idCuenta: '800024', saldo: 750000 });
+const cliente2 = new Cliente('000002', 'María', '654321', { idCuenta: '000034', saldo: 100000 }, { idCuenta: '800045', saldo: 250000 });
+const cliente3 = new Cliente('000003', 'Pedro', '333333', { idCuenta: '000056', saldo: 200000 }, { idCuenta: '800067', saldo: 350000 });
+const cliente4 = new Cliente('000004', 'Ana', '444444', { idCuenta: '000078', saldo: 150000 }, { idCuenta: '800089', saldo: 400000 });
 
-// Creación de una lista que emula una base de datos de clientes  
-const listaClientes = [cliente1, cliente2, cliente3, cliente4]
-
-const accesoClientes = listaClientes.map((cliente) => {
-    return {
-        id: cliente.id, 
-        password: cliente.password
-    }
-    })
+// Creación de una lista que emula una base de datos de clientes.
+const listaClientes = [cliente1, cliente2, cliente3, cliente4];
+const listaClientesJSON = JSON.stringify(listaClientes);
